686c34025b6e78d65794db2bcaea988fa21b62de
JonathanSaleh123 <jonathansaleh@berkeley.edu>
2023-11-06 23:13:42 -0800

    First Commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default[
+    {
+        title: 'Python Image/Text/Motion Detector',
+        imgUrl: 'src/assets/python.png',
+        stack: ['Python','OpenCV','PyTorch','Yolov8'],
+        link: 'https://github.com/JonathanSaleh123',
+    },
+    {
+        title: 'T-Prep Project - Trash Clasifier',
+        imgUrl: 'src/assets/T-prep.png',
+        stack: ['Express','HTML','Tensorflow'],
+        link: 'https://github.com/JonathanSaleh123',
+    },
+    {
+        title: 'Developer Portfolio',
+        imgUrl: 'src/assets/github.png',
+        stack: ['React','TailwindCSS','Javascript'],
+        link: 'https://github.com/JonathanSaleh123',
+    },
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Polysphere Backend Developer',
+        duration: '3 months',
+        details: 'Developing a website for a social media web-app that brings students with similar interests together',
+    },
+    {
+        year: 'Today',
+        title: 'UC Berkeley EECS Student',
+        duration: '5 months',
+        details: 'Studying EECS at Berkeley',
+    },
+    {
+        year: '2022',
+        title: 'Google Developer Student Clubs President',
+        duration: '12 months',
+        details: 'Managed a GDSC Chapter at Diablo Valley College',
+    },
+    {
+        year: '2021',
+        title: 'Diablo Valley College CS Student',
+        duration: '2 years',
+        details: 'CS Student at DVC',
+    }
+]
